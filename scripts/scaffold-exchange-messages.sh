f146ae00cf18175cfb05ffa360e01815e80edf73
loliallen <maxim.kasakin@yahoo.com>
2024-01-14 21:10:10 +0300

    added dex orders and exchange module

---
+++ scripts/scaffold-exchange-messages.sh
@@ -0,0 +1,4 @@
+ignite scaffold message create-pair sourceDenom targetDenom --module exchange
+ignite scaffold message send-buy-order amountDenom amount priceDenom price --module exchange
+ignite scaffold message send-sell-order amountDenom amount priceDenom price --module exchange
+
